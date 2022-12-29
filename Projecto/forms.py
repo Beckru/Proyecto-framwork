9e1da4bd0c4d921bc78a2bca4876307c13b1df22
Beckru <marcoschallapa@gmail.com>
2022-12-29 12:49:26 -0400

    creando tabla ususarios

---
+++ Projecto/forms.py
@@ -0,0 +1,8 @@
+from django import forms
+
+class registrarse():
+    first_name 
+    last_name
+    username
+    email
+    password
--- Projecto/models.py
+++ Projecto/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
 # Create your models here.
+
+class usuarios(models.Model):
+    first_name = models.CharField(max_length=150)
+    last_name = models.CharField(max_length=150)
+    email = models.CharField(max_length=150)
+    password= models.CharField(max_length=20)
+    is_staff= models.BooleanField(default=False)
+    is_estudent = models.BooleanField(default=False)
+    is_superuser=models.BooleanField(default=False)
