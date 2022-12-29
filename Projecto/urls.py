48dd2d2e29f9753b125d0133607da9bbd0eb1f25
Beckru <marcoschallapa@gmail.com>
2022-12-28 20:50:00 -0400

    usando html

--- Projecto/urls.py
+++ Projecto/urls.py
@@ -2,6 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.hello),
+    path('', views.inicio),
     path('about/', views.about),
 ]
--- Projecto/views.py
+++ Projecto/views.py
@@ -1,9 +1,10 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+#from .models import .... # aqui se ponen las tablas
 # Create your views here.
 
-def hello(request):
-    return HttpResponse("Hello World")
+def inicio(request):
+    return HttpResponse("inicio")
 
 def about(request):
     return HttpResponse("about")
